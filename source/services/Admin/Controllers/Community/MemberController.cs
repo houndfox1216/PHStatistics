6a5d788fb3d40bc8c145ab6006db93f60815b940
MSI\hound <leo@cloudfun.com.tw>
2024-05-20 11:17:25 +0800

    增加Member操作

---
+++ source/services/Admin/Controllers/Community/MemberController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Framework;
+using System.Framework.Data;
+using System.Framework.Web;
+using System.Linq;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using PHStatistics.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PHStatistics.Services.Admin.Controllers.Community {
+    [Route("api/[controller]")]
+    [EnableCors("AllPassOrigins")]
+    public class MemberController : ApiController<ServiceUser, ManagerModel, Culture> {
+
+        public ManagerController() : base("System") { }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
