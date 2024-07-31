82ec5b19e218466b40986d251bb3d39c8d964a6e
MSI\hound <leo@cloudfun.com.tw>
2024-07-31 09:21:03 +0800

    增加所屬公司

---
+++ source/schema/Data/Content/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHStatistics.Content {
+    /// <summary>
+    /// 班系所屬公司
+    /// </summary>
+    [Description("班系所屬公司")]
+    public enum Company : short {
+        /// <summary>
+        /// 百瀚
+        /// </summary>
+        [Display(Name = "百瀚")]
+        PH,
+
+        /// <summary>
+        /// 百世
+        /// </summary>
+        [Display(Name = "百世")]
+        PSJ,
+
+        /// <summary>
+        /// 其他
+        /// </summary>
+        [Display(Name = "其他")]
+        Other = 9,
+    }
+}
